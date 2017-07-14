cbcdda615af6cec2001fb2029896a237e4f3a14b
Samantha Morco <samanthamorco@gmail.com>
2017-07-13 21:13:45 -0400

    add new task

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -8,6 +8,9 @@
     $scope.message3 = "Sup World";
     $scope.tasks = ["Learn Angular", "Learn Vue", "Learn React"];
 
+    $scope.addTask = function(task) {
+      $scope.tasks.push(task);
+    };
     window.$scope = $scope;
   });
 })();
