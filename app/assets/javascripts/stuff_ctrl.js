a55f724eb9f1fd20894c91092dce2a0ac27cd180
Samantha Morco <samanthamorco@gmail.com>
2017-07-13 20:59:00 -0400

    ng repeat'

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -4,6 +4,9 @@
 
   angular.module("app").controller("stuffCtrl", function($scope) {
     $scope.message = "Hello World";
+    $scope.message2 = "Goodbye World";
+    $scope.message3 = "Sup World";
+    $scope.tasks = ["Learn Angular", "Learn Vue", "Learn React"];
 
     window.$scope = $scope;
   });
